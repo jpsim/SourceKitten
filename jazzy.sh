31aa827a45d797072b348a3501dabddc7ebaec39
JP Simard <jp@jpsim.com>
2014-10-31 20:29:32 -0700

    GitHub pages

--- jazzy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-jazzy -m sourcekitten \
--a "JP Simard" \
--u https://github.com/jpsim/sourcekitten \
--g https://github.com/jpsim/sourcekitten \
---github-file-prefix https://github.com/jpsim/sourcekitten/blob/0.1.0
