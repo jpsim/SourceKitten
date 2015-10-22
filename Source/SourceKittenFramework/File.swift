63bf79d74d072fd3b77208d1853810d75e2bf9fc
JP Simard <jp@jpsim.com>
2015-10-21 20:58:26 -0400

    remove a few self. references in File

--- Source/SourceKittenFramework/File.swift
+++ Source/SourceKittenFramework/File.swift
@@ -27,8 +27,8 @@ public struct File {
     public init?(path: String) {
         self.path = path
         do {
-            self.contents = try NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding) as String
-            self.lines = self.contents.lines()
+            contents = try NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding) as String
+            lines = contents.lines()
         } catch {
             fputs("Could not read contents of `\(path)`\n", stderr)
             return nil
@@ -43,7 +43,7 @@ public struct File {
     public init(contents: String) {
         path = nil
         self.contents = contents
-        self.lines = self.contents.lines()
+        lines = contents.lines()
     }
 
     /**
