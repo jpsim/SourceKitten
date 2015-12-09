4646514f85e83aa06b86f63f9a3b92ad1de2f663
Minh Nguyễn <mxn@1ec5.org>
2015-12-09 02:37:03 -0800

    Added test for Unicode in Objective-C

---
+++ Source/SourceKittenFrameworkTests/Fixtures/SuperScript.h
@@ -0,0 +1,15 @@
+#import <Foundation/Foundation.h>
+
+/**
+ ᴬ 	ᴮ 		ᴰ 	ᴱ 		ᴳ 	ᴴ 	ᴵ 	ᴶ 	ᴷ 	ᴸ 	ᴹ 	ᴺ 	ᴼ 	ᴾ 		ᴿ 		ᵀ 	ᵁ 	ⱽ 	ᵂ 		
+ */
+@interface SuperScript : NSObject
+
+#pragma mark ᵃ	ᵇ	ᶜ	ᵈ	ᵉ	ᶠ	ᵍ	ʰ	ⁱ	ʲ	ᵏ	ˡ	ᵐ	ⁿ	ᵒ	ᵖ		ʳ	ˢ	ᵗ	ᵘ	ᵛ	ʷ	ˣ	ʸ	ᶻ
+
+/**
+  	ᵝ 	ᵞ 	ᵟ 	ᵋ 			ᶿ 	ᶥ 											ᶹ 	ᵠ 	ᵡ
+ */
+- (IBAction)superSize:(id)sender;
+
+@end
