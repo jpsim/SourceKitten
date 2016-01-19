f0c595e1809ec597b8e093a0972dbc8eab249e65
JP Simard <jp@jpsim.com>
2016-01-19 00:32:22 -0800

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+import PackageDescription
+
+let package = Package(
+  name: "SourceKitten",
+  targets: [
+    Target(name: "SourceKittenFramework"),
+    Target(name: "sourcekitten",
+      dependencies: [.Target(name: "SourceKittenFramework")])
+  ],
+  dependencies: [
+    .Package(url: "https://github.com/jpsim/SourceKit.git", majorVersion: 1),
+    .Package(url: "https://github.com/jpsim/SWXMLHash.git", majorVersion: 1),
+    .Package(url: "https://github.com/jpsim/Commandant.git", majorVersion: 1)
+  ],
+  exclude: ["Source/SourceKittenFrameworkTests", "Carthage"]
+)
