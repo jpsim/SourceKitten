3f773f5f54c191f7d34e54d570b58d743dd8c744
JP Simard <jp@jpsim.com>
2016-02-08 16:51:33 -0800

    re-add the '.git' in Package.swift dependency URLs

--- Package.swift
+++ Package.swift
@@ -10,9 +10,9 @@ let package = Package(
       dependencies: [.Target(name: "SourceKittenFramework")]),
   ],
   dependencies: [
-    .Package(url: "https://github.com/jpsim/SourceKit", majorVersion: 1),
-    .Package(url: "https://github.com/drmohundro/SWXMLHash", majorVersion: 2),
-    .Package(url: "https://github.com/Carthage/Commandant", majorVersion: 0, minor: 8),
-    .Package(url: "https://github.com/norio-nomura/swift-corelibs-xctest", majorVersion: 0),
+    .Package(url: "https://github.com/jpsim/SourceKit.git", majorVersion: 1),
+    .Package(url: "https://github.com/drmohundro/SWXMLHash.git", majorVersion: 2),
+    .Package(url: "https://github.com/Carthage/Commandant.git", majorVersion: 0, minor: 8),
+    .Package(url: "https://github.com/norio-nomura/swift-corelibs-xctest.git", majorVersion: 0),
   ]
 )
