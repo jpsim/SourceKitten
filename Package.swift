44bec4d45aefdcb18f2f0dadf0748bfef32a8f15
Norio Nomura <norio.nomura@gmail.com>
2016-02-21 00:59:12 +0900

    [SPM] Change `SourceKit` to point forked url that stopped linking `sourcekitdInProc`

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
   ],
   dependencies: [
     .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 1),
-    .Package(url: "https://github.com/jpsim/SourceKit.git", majorVersion: 1),
+    .Package(url: "https://github.com/norio-nomura/SourceKit.git", majorVersion: 1),
     .Package(url: "https://github.com/drmohundro/SWXMLHash.git", majorVersion: 2),
     .Package(url: "https://github.com/Carthage/Commandant.git", majorVersion: 0, minor: 8),
     .Package(url: "https://github.com/norio-nomura/swift-corelibs-xctest.git", majorVersion: 0),
