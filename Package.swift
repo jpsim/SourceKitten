29ace5cb166fbe82e1ad0223cfeb01fd5db0572f
Norio Nomura <norio.nomura@gmail.com>
2016-02-21 15:20:15 +0900

    [SPM] Add `Clang_C` as dependency to `Package.swift`
    By applying this, `make spm_bootstrap` is no longer needed.

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 1),
     .Package(url: "https://github.com/norio-nomura/SourceKit.git", majorVersion: 1),
+    .Package(url: "https://github.com/norio-nomura/Clang_C.git", majorVersion: 1),
     .Package(url: "https://github.com/drmohundro/SWXMLHash.git", majorVersion: 2),
     .Package(url: "https://github.com/Carthage/Commandant.git", majorVersion: 0, minor: 8),
     .Package(url: "https://github.com/norio-nomura/swift-corelibs-xctest.git", majorVersion: 0),
