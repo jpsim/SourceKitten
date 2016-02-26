ce7391abfde96c88b8b2c430ab03d4d31d0746f8
Norio Nomura <norio.nomura@gmail.com>
2016-02-26 13:05:09 +0900

    Use `interfacegen` instead of manually created .txt of Generated Swift Interface

--- Source/SourceKittenFramework/library_wrapper/generate_library_wrappers.sh
+++ Source/SourceKittenFramework/library_wrapper/generate_library_wrappers.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-[ -z "$SRCROOT" ] && exit
+[ -z "$SRCROOT" -o -z "$TARGET_TEMP_DIR" ] && exit 1
 
 cd $SRCROOT/Source/SourceKittenFramework/
 converter=$SRCROOT/Source/SourceKittenFramework/library_wrapper/convert_generated_interface_to_wrapper.py
+interfacegen_sourcekitd=$SRCROOT/Source/SourceKittenFramework/library_wrapper/interfacegen_sourcekitd.sh
+interfacegen_Clang_C=$SRCROOT/Source/SourceKittenFramework/library_wrapper/interfacegen_Clang_C.sh
 
 # sourcekitd.framework
-$converter library_wrapper/sourcekitd_h.txt sourcekitd.framework/Versions/A/sourcekitd SourceKit > library_wrapper_sourcekitd.swift
+SOURCEKITD_GENERATED_INTERFACE=$TARGET_TEMP_DIR/sourcekitd_h.txt
+$interfacegen_sourcekitd > $SOURCEKITD_GENERATED_INTERFACE
+$converter $SOURCEKITD_GENERATED_INTERFACE sourcekitd.framework/Versions/A/sourcekitd SourceKit > library_wrapper_sourcekitd.swift
 
 # libclang.dylib
 for f in CXString Documentation Index; do
-    $converter library_wrapper/${f}_h.txt libclang.dylib Clang_C > library_wrapper_${f}.swift
+  Clang_C_GENERATED_INTERFACE=$TARGET_TEMP_DIR/${f}_h.txt
+  $interfacegen_Clang_C $f > $Clang_C_GENERATED_INTERFACE
+  $converter $Clang_C_GENERATED_INTERFACE libclang.dylib Clang_C > library_wrapper_${f}.swift
 done
---
+++ Source/SourceKittenFramework/library_wrapper/interfacegen_sourcekitd.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+[ -z "$SRCROOT" -o -z "$TARGET_BUILD_DIR" -o -z "$TARGET_TEMP_DIR" ] && exit 1
+
+interfacegen=$TARGET_BUILD_DIR/interfacegen
+
+[ -x $interfacegen ] || exit 1
+
+HEADER_PATH=$SRCROOT/Source/SourceKittenFramework
+HEADER=$HEADER_PATH/sourcekitd.h
+INTERFACEGEN_TEMPORARY_SOURCE=$TARGET_TEMP_DIR/interfacegen_sourcekitd.m
+
+echo "#include \"$HEADER\"" > $INTERFACEGEN_TEMPORARY_SOURCE
+$interfacegen -header $HEADER -- -fsyntax-only $INTERFACEGEN_TEMPORARY_SOURCE -I $HEADER_PATH
