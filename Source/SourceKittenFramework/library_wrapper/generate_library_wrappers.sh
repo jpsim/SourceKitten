4f9352ff65eb01a09d75ef38374491e48a7cddd1
JP Simard <jp@jpsim.com>
2016-03-16 18:38:33 -0700

    reduced library_wrapper_sourcekitd.swift to bare minimum

--- Source/SourceKittenFramework/library_wrapper/generate_library_wrappers.sh
+++ Source/SourceKittenFramework/library_wrapper/generate_library_wrappers.sh
@@ -7,14 +7,14 @@ converter=$SRCROOT/Source/SourceKittenFramework/library_wrapper/convert_generate
 interfacegen_sourcekitd=$SRCROOT/Source/SourceKittenFramework/library_wrapper/interfacegen_sourcekitd.sh
 interfacegen_Clang_C=$SRCROOT/Source/SourceKittenFramework/library_wrapper/interfacegen_Clang_C.sh
 
-# sourcekitd.framework
-SOURCEKITD_GENERATED_INTERFACE=$TARGET_TEMP_DIR/sourcekitd_h.txt
-$interfacegen_sourcekitd > $SOURCEKITD_GENERATED_INTERFACE || exit 1
-$converter $SOURCEKITD_GENERATED_INTERFACE sourcekitd.framework/Versions/A/sourcekitd SourceKit > library_wrapper_sourcekitd.swift
+# # sourcekitd.framework
+# SOURCEKITD_GENERATED_INTERFACE=$TARGET_TEMP_DIR/sourcekitd_h.txt
+# $interfacegen_sourcekitd > $SOURCEKITD_GENERATED_INTERFACE || exit 1
+# $converter $SOURCEKITD_GENERATED_INTERFACE sourcekitd.framework/Versions/A/sourcekitd SourceKit > library_wrapper_sourcekitd.swift
 
-# libclang.dylib
-for f in CXString Documentation Index; do
-  Clang_C_GENERATED_INTERFACE=$TARGET_TEMP_DIR/${f}_h.txt
-  $interfacegen_Clang_C $f > $Clang_C_GENERATED_INTERFACE || exit 1
-  $converter $Clang_C_GENERATED_INTERFACE libclang.dylib Clang_C > library_wrapper_${f}.swift
-done
+# # libclang.dylib
+# for f in CXString Documentation Index; do
+#   Clang_C_GENERATED_INTERFACE=$TARGET_TEMP_DIR/${f}_h.txt
+#   $interfacegen_Clang_C $f > $Clang_C_GENERATED_INTERFACE || exit 1
+#   $converter $Clang_C_GENERATED_INTERFACE libclang.dylib Clang_C > library_wrapper_${f}.swift
+# done
