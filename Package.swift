e59497c03df0309e9a6908bfb9452b4ae684c992
JP Simard <jp@jpsim.com>
2016-05-05 19:08:12 +0100

    update Package.swift dependencies

--- Package.swift
+++ Package.swift
@@ -8,10 +8,10 @@ let package = Package(
       dependencies: [.Target(name: "SourceKittenFramework")]),
   ],
   dependencies: [
-    .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 1),
+    .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 2),
     .Package(url: "https://github.com/norio-nomura/SourceKit.git", majorVersion: 1),
     .Package(url: "https://github.com/norio-nomura/Clang_C.git", majorVersion: 1),
-    .Package(url: "https://github.com/drmohundro/SWXMLHash.git", majorVersion: 2, minor: 2),
-    .Package(url: "https://github.com/Carthage/Commandant.git", majorVersion: 0, minor: 9),
+    .Package(url: "https://github.com/drmohundro/SWXMLHash.git", majorVersion: 2, minor: 3),
+    .Package(url: "https://github.com/Carthage/Commandant.git", majorVersion: 0, minor: 10),
   ]
 )
