071866f051c5dd923757b98e70bdd0df3585db25
JP Simard <jp@jpsim.com>
2016-06-30 12:26:57 -0400

    re-add StructureCommand

--- Source/sourcekitten/main.swift
+++ Source/sourcekitten/main.swift
@@ -16,6 +16,7 @@ import SourceKittenFramework
 
 let registry = CommandRegistry<SourceKittenError>()
 registry.register(command: SyntaxCommand())
+registry.register(command: StructureCommand())
 registry.register(command: VersionCommand())
 
 let helpCommand = HelpCommand(registry: registry)
