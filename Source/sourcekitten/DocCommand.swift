2a631b0e0e69202d4ba2dce1b4647b5769ed35b3
JP Simard <jp@jpsim.com>
2016-06-30 12:36:32 -0400

    re-add DocCommand stub

--- Source/sourcekitten/DocCommand.swift
+++ Source/sourcekitten/DocCommand.swift
@@ -1,1 +1,55 @@
+//
+//  DocCommand.swift
+//  SourceKitten
+//
+//  Created by JP Simard on 2015-01-07.
+//  Copyright (c) 2015 SourceKitten. All rights reserved.
+//
 
+import Commandant
+import Foundation
+import Result
+import SourceKittenFramework
+
+struct DocCommand: CommandType {
+    let verb = "doc"
+    let function = "Print Swift docs as JSON or Objective-C docs as XML"
+
+    struct Options: OptionsType {
+        let spmModule: String
+        let singleFile: Bool
+        let moduleName: String
+        let objc: Bool
+        let arguments: [String]
+
+        static func create(spmModule: String) -> (singleFile: Bool) -> (moduleName: String) -> (objc: Bool) -> (arguments: [String]) -> Options {
+            return { singleFile in { moduleName in { objc in { arguments in
+                self.init(spmModule: spmModule, singleFile: singleFile, moduleName: moduleName, objc: objc, arguments: arguments)
+            }}}}
+        }
+
+        static func evaluate(_ m: CommandMode) -> Result<Options, CommandantError<SourceKittenError>> {
+            return create
+                <*> m <| Option(key: "spm-module", defaultValue: "", usage: "document a Swift Package Manager module")
+                <*> m <| Option(key: "single-file", defaultValue: false, usage: "only document one file")
+                <*> m <| Option(key: "module-name", defaultValue: "",    usage: "name of module to document (can't be used with `--single-file` or `--objc`)")
+                <*> m <| Option(key: "objc",        defaultValue: false, usage: "document Objective-C headers")
+                <*> m <| Argument(defaultValue: [], usage: "Arguments list that passed to xcodebuild. If `-` prefixed argument exists, place ` -- ` before that.")
+        }
+    }
+
+    func run(_ options: Options) -> Result<(), SourceKittenError> {
+        return .success()
+    }
+
+#if !os(Linux)
+    func runObjC(options: Options, args: [String]) -> Result<(), SourceKittenError> {
+        if args.isEmpty {
+            return .failure(.InvalidArgument(description: "at least 5 arguments are required when using `--objc`"))
+        }
+        let translationUnit = ClangTranslationUnit(headerFiles: [args[0]], compilerArguments: Array(args.dropFirst(1)))
+        print(translationUnit)
+        return .success()
+    }
+#endif
+}
--- Source/sourcekitten/main.swift
+++ Source/sourcekitten/main.swift
@@ -16,7 +16,7 @@ import SourceKittenFramework
 
 let registry = CommandRegistry<SourceKittenError>()
 registry.register(command: CompleteCommand())
-//registry.register(command: DocCommand())
+registry.register(command: DocCommand())
 registry.register(command: FormatCommand())
 registry.register(command: IndexCommand())
 registry.register(command: SyntaxCommand())
