e050abcc75b775f60299470b2f27bde0ccf4036c
JP Simard <jp@jpsim.com>
2016-06-30 12:09:21 -0400

    re-integrate Commandant

--- Package.swift
+++ Package.swift
@@ -2,21 +2,6 @@ import PackageDescription
 
 let dependencies: [Package.Dependency]
 
-#if os(Linux)
-dependencies = [
-  .Package(url: "https://github.com/norio-nomura/SourceKit.git", majorVersion: 1),
-]
-#else
-dependencies = [
-  .Package(url: "https://github.com/norio-nomura/SourceKit.git", majorVersion: 1),
-  .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 1),
-  .Package(url: "https://github.com/norio-nomura/Clang_C.git", majorVersion: 1),
-  .Package(url: "https://github.com/drmohundro/SWXMLHash.git", majorVersion: 2, minor: 3),
-  .Package(url: "https://github.com/Carthage/Commandant.git", majorVersion: 0, minor: 9),
-  .Package(url: "https://github.com/behrang/YamlSwift.git", majorVersion: 1, minor: 4),
-]
-#endif
-
 let package = Package(
   name: "SourceKitten",
   targets: [
@@ -24,6 +9,9 @@ let package = Package(
     Target(name: "sourcekitten",
       dependencies: [.Target(name: "SourceKittenFramework")]),
   ],
-  dependencies: dependencies,
+  dependencies: [
+    .Package(url: "https://github.com/norio-nomura/SourceKit.git", majorVersion: 1),
+    .Package(url: "https://github.com/jpsim/Commandant.git", majorVersion: 5),
+  ],
   exclude: ["Tests/SourceKittenFramework/Fixtures"]
 )
