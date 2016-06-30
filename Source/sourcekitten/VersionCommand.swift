e5a61cfe24b1b17be11d2b727b62ea160fcf987d
JP Simard <jp@jpsim.com>
2016-06-30 12:18:32 -0400

    re-integrate Commandant into Xcode and sourcekitten

--- Source/sourcekitten/VersionCommand.swift
+++ Source/sourcekitten/VersionCommand.swift
@@ -1,1 +1,22 @@
+//
+//  VersionCommand.swift
+//  SourceKitten
+//
+//  Created by JP Simard on 2015-01-07.
+//  Copyright (c) 2015 SourceKitten. All rights reserved.
+//
 
+import Commandant
+import Result
+
+private let version = "0.13.0"
+
+struct VersionCommand: CommandType {
+    let verb = "version"
+    let function = "Display the current version of SourceKitten"
+
+    func run(_ options: NoOptions<SourceKittenError>) -> Result<(), SourceKittenError> {
+        print(version)
+        return .success()
+    }
+}
--- Source/sourcekitten/main.swift
+++ Source/sourcekitten/main.swift
@@ -5,6 +5,17 @@
 //  Created by JP Simard on 2015-01-03.
 //  Copyright (c) 2015 SourceKitten. All rights reserved.
 //
+
+import Commandant
 import SourceKittenFramework
 
-print(SyntaxMap(file: File(path: Process.arguments[1])!))
+//print(SyntaxMap(file: File(path: Process.arguments[1])!))
+let registry = CommandRegistry<SourceKittenError>()
+registry.register(command: VersionCommand())
+
+let helpCommand = HelpCommand(registry: registry)
+registry.register(command: helpCommand)
+
+registry.main(defaultVerb: "help") { error in
+    fputs("\(error)\n", stderr)
+}
