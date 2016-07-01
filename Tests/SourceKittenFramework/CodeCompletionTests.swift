ad12893750254912067f14aeaafc1820cf1d56b4
JP Simard <jp@jpsim.com>
2016-07-01 16:44:40 -0400

    compile more of CodeCompletionTests on Linux

--- Tests/SourceKittenFramework/CodeCompletionTests.swift
+++ Tests/SourceKittenFramework/CodeCompletionTests.swift
@@ -6,8 +6,6 @@
 //  Copyright © 2015 SourceKitten. All rights reserved.
 //
 
-#if !os(Linux)
-
 import Foundation
 import SourceKittenFramework
 import XCTest
@@ -15,13 +13,17 @@ import XCTest
 class CodeCompletionTests: XCTestCase {
 
     func testSimpleCodeCompletion() {
+        #if os(Linux)
+        let file = "\(NSUUID().UUIDString).swift"
+        #else
         let file = "\(NSUUID().uuidString).swift"
+        #endif
         let completionItems = CodeCompletionItem.parseResponse(
             response: Request.CodeCompletionRequest(file: file, contents: "0.", offset: 2,
                 arguments: ["-c", file, "-sdk", sdkPath()]).send())
+        #if !os(Linux)
         compareJSONStringWithFixturesName("SimpleCodeCompletion",
             jsonString: completionItems)
+        #endif
     }
 }
-
-#endif
