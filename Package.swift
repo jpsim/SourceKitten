00de65dfc77a0c7a1320068cdf00fcd4b2dd0f23
JP Simard <jp@jpsim.com>
2016-07-05 10:18:48 -0400

    re-add Clang_C to Package.swift and sort packages

--- Package.swift
+++ Package.swift
@@ -8,10 +8,11 @@ let package = Package(
       dependencies: [.Target(name: "SourceKittenFramework")]),
   ],
   dependencies: [
-    .Package(url: "https://github.com/norio-nomura/SourceKit.git", majorVersion: 1),
     .Package(url: "https://github.com/jpsim/Commandant.git", majorVersion: 5),
-    .Package(url: "https://github.com/jpsim/YamlSwift.git", majorVersion: 1, minor: 6),
     .Package(url: "https://github.com/jpsim/SWXMLHash.git", majorVersion: 2, minor: 4),
+    .Package(url: "https://github.com/jpsim/YamlSwift.git", majorVersion: 1, minor: 6),
+    .Package(url: "https://github.com/norio-nomura/Clang_C.git", majorVersion: 1),
+    .Package(url: "https://github.com/norio-nomura/SourceKit.git", majorVersion: 1),
   ],
   exclude: ["Tests/SourceKittenFramework/Fixtures"]
 )
