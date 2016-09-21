df0ffbf460a5c50923d30c2191afbebe0b3db77c
JP Simard <jp@jpsim.com>
2016-09-20 18:10:56 -0700

    update library wrappers for Swift 3

--- Source/SourceKittenFramework/library_wrapper_CXString.swift
+++ Source/SourceKittenFramework/library_wrapper_CXString.swift
@@ -2,5 +2,5 @@
 import Clang_C
 #endif
 private let library = toolchainLoader.load("libclang.dylib")
-internal let clang_getCString: @convention(c) (CXString) -> (UnsafePointer<Int8>) = library.loadSymbol("clang_getCString")
+internal let clang_getCString: @convention(c) (CXString) -> (UnsafePointer<Int8>!) = library.loadSymbol("clang_getCString")
 internal let clang_disposeString: @convention(c) (CXString) -> () = library.loadSymbol("clang_disposeString")
