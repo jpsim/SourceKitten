fcca4a49c4e7590ffa7c94d760f5c0a5dd6ddd9e
JP Simard <jp@jpsim.com>
2016-10-12 16:04:21 -0700

    make NSString.absolutePathRepresentation(...) compile on Linux

--- Source/SourceKittenFramework/String+SourceKitten.swift
+++ Source/SourceKittenFramework/String+SourceKitten.swift
@@ -245,7 +245,11 @@ extension NSString {
     */
     public func absolutePathRepresentation(rootDirectory: String = FileManager.default.currentDirectoryPath) -> String {
         if isAbsolutePath { return bridge() }
+        #if os(Linux)
+        return NSURL(fileURLWithPath: NSURL.fileURL(withPathComponents: [rootDirectory, bridge()])!.path).standardizingPath!.path
+        #else
         return NSString.path(withComponents: [rootDirectory, bridge()]).bridge().standardizingPath
+        #endif
     }
 
     /**
