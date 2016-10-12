88ebc3d75feed33beb6fb7fad10a90d73583c1e5
JP Simard <jp@jpsim.com>
2016-10-12 14:29:56 -0700

    add Tests/LinuxMain.swit

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,16 @@
+import XCTest
+
+@testable import SourceKittenFrameworkTests
+
+XCTMain([
+    // testCase(ClangTranslationUnitTests.allTests),
+    // testCase(CodeCompletionTests.allTests),
+    testCase(FileTests.allTests),
+    // testCase(ModuleTests.allTests),
+    testCase(OffsetMapTests.allTests),
+    testCase(SourceKitTests.allTests),
+    testCase(StringTests.allTests),
+    testCase(StructureTests.allTests),
+    // testCase(SwiftDocsTests.allTests),
+    testCase(SyntaxTests.allTests),
+])
