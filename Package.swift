e3ba66a5ba4938ca98e002a27bc98725f6c176af
Norio Nomura <norio.nomura@gmail.com>
2017-01-31 09:17:58 +0900

    Update Yams to 0.1.5

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/Carthage/Commandant.git", versions: Version(0, 11, 3)..<Version(0, 11, .max)),
     .Package(url: "https://github.com/drmohundro/SWXMLHash.git", Version(3, 0, 3)),
-    .Package(url: "https://github.com/jpsim/Yams.git", Version(0, 1, 4)),
+    .Package(url: "https://github.com/jpsim/Yams.git", Version(0, 1, 5)),
     .Package(url: "https://github.com/norio-nomura/Clang_C.git", majorVersion: 1),
     .Package(url: "https://github.com/norio-nomura/SourceKit.git", majorVersion: 1),
   ],
