bc641153e93030e905e09eb379860ef8c1257a62
JP Simard <jp@jpsim.com>
2017-03-31 11:17:23 -0700

    update Yams to 0.3.0

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/Carthage/Commandant.git", versions: Version(0, 12, 0)..<Version(0, 12, .max)),
     .Package(url: "https://github.com/drmohundro/SWXMLHash.git", Version(3, 0, 4)),
-    .Package(url: "https://github.com/jpsim/Yams.git", Version(0, 2, 0)),
+    .Package(url: "https://github.com/jpsim/Yams.git", Version(0, 3, 0)),
     .Package(url: "https://github.com/norio-nomura/Clang_C.git", majorVersion: 1),
     .Package(url: "https://github.com/norio-nomura/SourceKit.git", majorVersion: 1),
   ],
