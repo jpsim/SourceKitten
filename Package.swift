fd6b1523293faaaba30ccf4c2e4650089a0fd79e
JP Simard <jp@jpsim.com>
2017-05-26 16:08:19 -0700

    point to local repos

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
   ],
   dependencies: [
     .Package(url: "https://github.com/Carthage/Commandant.git", versions: Version(0, 12, 0)..<Version(0, 12, .max)),
-    .Package(url: "https://github.com/drmohundro/SWXMLHash.git", "version-4.0-changes"),
-    .Package(url: "https://github.com/jpsim/Yams.git", branch: "jp-swift-4"),
+    .Package(url: "/Users/jp/Projects/SWXMLHash", majorVersion: 4),
+    .Package(url: "/Users/jp/Projects/Yams", majorVersion: 1),
     .Package(url: "https://github.com/norio-nomura/Clang_C.git", majorVersion: 1),
     .Package(url: "https://github.com/norio-nomura/SourceKit.git", majorVersion: 1),
   ],
