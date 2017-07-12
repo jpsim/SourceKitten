6f190cc00272e677feb2838e8684681580bf74c9
JP Simard <jp@jpsim.com>
2017-07-12 09:54:36 -0700

    reformat LinuxCompatibility.swift

--- Source/SourceKittenFramework/LinuxCompatibility.swift
+++ Source/SourceKittenFramework/LinuxCompatibility.swift
@@ -8,62 +8,60 @@
 
 import Foundation
 
-#if !os(Linux)
-    #if !swift(>=4.0)
-        extension NSTextCheckingResult {
-            func range(at idx: Int) -> NSRange {
-                return rangeAt(idx)
-            }
-        }
-    #endif
+#if !os(Linux) && !swift(>=4.0)
+extension NSTextCheckingResult {
+    func range(at idx: Int) -> NSRange {
+        return rangeAt(idx)
+    }
+}
 #endif
 
 extension Array {
     public func bridge() -> NSArray {
-        #if os(Linux)
-            return NSArray(array: self)
-        #else
-            return self as NSArray
-        #endif
+#if os(Linux)
+        return NSArray(array: self)
+#else
+        return self as NSArray
+#endif
     }
 }
 
 extension CharacterSet {
     public func bridge() -> NSCharacterSet {
-        #if os(Linux)
-            return _bridgeToObjectiveC()
-        #else
-            return self as NSCharacterSet
-        #endif
+#if os(Linux)
+        return _bridgeToObjectiveC()
+#else
+        return self as NSCharacterSet
+#endif
     }
 }
 
 extension Dictionary {
     public func bridge() -> NSDictionary {
-        #if os(Linux)
-            return NSDictionary(dictionary: self)
-        #else
-            return self as NSDictionary
-        #endif
+#if os(Linux)
+        return NSDictionary(dictionary: self)
+#else
+        return self as NSDictionary
+#endif
     }
 }
 
 extension NSString {
     public func bridge() -> String {
-        #if os(Linux)
-            return _bridgeToSwift()
-        #else
-            return self as String
-        #endif
+#if os(Linux)
+        return _bridgeToSwift()
+#else
+        return self as String
+#endif
     }
 }
 
 extension String {
     public func bridge() -> NSString {
-        #if os(Linux)
-            return NSString(string: self)
-        #else
-            return self as NSString
-        #endif
+#if os(Linux)
+        return NSString(string: self)
+#else
+        return self as NSString
+#endif
     }
 }
