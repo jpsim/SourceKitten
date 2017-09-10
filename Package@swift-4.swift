9f3015ba5010968c2091651b2771dbff91fb3182
JP Simard <jp@jpsim.com>
2017-09-09 17:03:34 -0700

    Update SWXMLHash and Yams to their latest versions

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -9,8 +9,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Carthage/Commandant.git", from: "0.12.0"),
-        .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "4.1.1"),
-        .package(url: "https://github.com/jpsim/Yams.git", from: "0.3.4"),
+        .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "4.2.0"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "0.3.6"),
         .package(url: "https://github.com/norio-nomura/Clang_C.git", from: "1.0.0"),
         .package(url: "https://github.com/norio-nomura/SourceKit.git", from: "1.0.0"),
     ],
