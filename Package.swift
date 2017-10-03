ea1dbc8f2eea7a55ba2690109e6a49295ee833c5
Eneko Alonso <eneko.alonso@gmail.com>
2017-10-03 11:21:01 -0700

    Remove Package.resolved, add back Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+import PackageDescription
+
+let package = Package(
+  name: "SourceKitten",
+  targets: [
+    Target(name: "SourceKittenFramework"),
+    Target(name: "sourcekitten",
+      dependencies: [.Target(name: "SourceKittenFramework")]),
+  ],
+  dependencies: [
+    .Package(url: "https://github.com/Carthage/Commandant.git", majorVersion: 0, minor: 12),
+    .Package(url: "https://github.com/drmohundro/SWXMLHash.git", majorVersion: 4),
+    .Package(url: "https://github.com/jpsim/Yams.git", majorVersion: 0, minor: 3),
+    .Package(url: "https://github.com/norio-nomura/Clang_C.git", majorVersion: 1),
+    .Package(url: "https://github.com/norio-nomura/SourceKit.git", majorVersion: 1),
+  ],
+  swiftLanguageVersions: [3, 4],
+  exclude: [
+    "Source/SourceKittenFramework/clang-c",
+    "Source/SourceKittenFramework/sourcekitd.h",
+    "Tests/SourceKittenFrameworkTests/Fixtures",
+  ]
+)
