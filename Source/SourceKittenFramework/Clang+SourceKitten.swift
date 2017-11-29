459cfb8ef1b465bf31abce731bd7136ac0f834b2
John Fairhurst <johnfairh@gmail.com>
2017-11-29 15:37:51 +0000

    Fix generic Swift declaration generation from ObjC

--- Source/SourceKittenFramework/Clang+SourceKitten.swift
+++ Source/SourceKittenFramework/Clang+SourceKitten.swift
@@ -212,21 +212,9 @@ extension CXCursor {
             return nil
         }
 
-        // SourceKit with Xcode 9+ doesn't escape ampersands in its XML output, so let's sanitize the XML ourselves.
-        func fixAmpersandInXML(_ xml: String) -> String {
-            return xml.replacingOccurrences(of: " & ", with: " &amp; ")
-        }
-
         let cursorInfo = Request.cursorInfo(file: swiftUUID, offset: usrOffset, arguments: compilerArguments).send()
-        if let docsXML = cursorInfo[SwiftDocKey.fullXMLDocs.rawValue] as? String,
-            case let fixedDocsXML = fixAmpersandInXML(docsXML),
-           let swiftDeclaration = SWXMLHash.parse(fixedDocsXML).children.first?["Declaration"].element?.text {
-                return swiftDeclaration
-        }
-
         if let annotatedDeclarationXML = cursorInfo[SwiftDocKey.annotatedDeclaration.rawValue] as? String,
-            case let fixedAnnotatedDeclarationXML = fixAmpersandInXML(annotatedDeclarationXML),
-           let swiftDeclaration = SWXMLHash.parse(fixedAnnotatedDeclarationXML).element?.recursiveText {
+           let swiftDeclaration = SWXMLHash.parse(annotatedDeclarationXML).element?.recursiveText {
                 return swiftDeclaration
         }
 
