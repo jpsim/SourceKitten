d9cd79e8f50bffc2d13848e5348f1f988278b2b5
JP Simard <jp@jpsim.com>
2018-02-01 11:03:16 -0800

    Pin Commandant based on revision rather than branch
    so that downstream SPM consumers don't get warnings

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "SourceKittenFramework", targets: ["SourceKittenFramework"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Carthage/Commandant.git", .branch("master")),
+        .package(url: "https://github.com/Carthage/Commandant.git", .revision("ab5f20d3e845dc3845eb4c79a4bd8d5ac8af5e1c")),
         .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "4.2.0"),
         .package(url: "https://github.com/jpsim/Yams.git", .upToNextMinor(from: "0.5.0") ),
         .package(url: "https://github.com/norio-nomura/Clang_C.git", from: "1.0.0"),
