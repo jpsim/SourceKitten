309df7c29a716691f486465b74d9e8e91c132d25
Timofey Solonin <abdulowork@gmail.com>
2019-05-14 17:36:20 +0300

    Add request for fetching cursorInfo from USR (#596)
    * #595 - Add cursorInfoUSR request case
    
    * #595 - Add cursorInfoUSR tests
    
    * Update CHANGELOG
    
    * #595 - Separate fixtures by swift versions

---
+++ Tests/SourceKittenFrameworkTests/CursorInfoUSRTests.swift
@@ -0,0 +1,37 @@
+//
+//  CursorInfoUSRTests.swift
+//  SourceKittenFrameworkTests
+//
+//  Created by Timofey Solonin on 14/05/19.
+//  Copyright © 2019 SourceKitten. All rights reserved.
+//
+
+import Foundation
+import SourceKittenFramework
+import XCTest
+
+class CursorInfoUSRTests: XCTestCase {
+
+    /// Validates that cursorInfo is parsed correctly.
+    func testCursorInfoUSRRequest() throws {
+        let path = fixturesDirectory + "DocInfo.swift"
+
+        let info = toNSDictionary(
+            try Request.cursorInfoUSR(
+                file: path,
+                usr: "s:7DocInfo22multiLineCommentedFuncyyF",
+                arguments: ["-sdk", sdkPath(), path],
+                cancelOnSubsequentRequest: false
+            ).send()
+        )
+        compareJSONString(withFixtureNamed: "CursorInfoUSR", jsonString: toJSON(info))
+    }
+}
+
+extension CursorInfoUSRTests {
+    static var allTests: [(String, (CursorInfoUSRTests) -> () throws -> Void)] {
+        return [
+            ("testCursorInfoUSRRequest", testCursorInfoUSRRequest)
+        ]
+    }
+}
