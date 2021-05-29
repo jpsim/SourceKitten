fe4d863c7aed7fd83591b6d4c2bf3aa665ca207f
John Fairhurst <johnfairh@gmail.com>
2021-05-29 11:49:26 +0100

    Decl attribute changes

--- Tests/SourceKittenFrameworkTests/SourceKitTests.swift
+++ Tests/SourceKittenFrameworkTests/SourceKitTests.swift
@@ -118,6 +118,17 @@ class SourceKitTests: XCTestCase {
         let actual = sourcekitStrings(startingWith: "source.decl.attribute.")
             .subtracting(attributesFoundInSwift5ButWeIgnore)
 
+#if compiler(>=5.4)
+        // removed in Swift 5.4
+        expected.subtract([._functionBuilder])
+#else
+        // added in Swift 5.4
+        expected.subtract([
+            ._specializeExtension, .actor, .actorIndepedent, .async, .asyncHandler,
+            .globalActor, .resultBuilder
+        ])
+#endif
+
 #if compiler(>=5.3)
 #else
         // added in Swift 5.3
