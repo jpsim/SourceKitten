48f15e0ce8f44e1d4d54e30273662149c9281671
Galen Rhodes <galenrhodes@me.com>
2021-10-25 06:31:27 -0400

    Speed up Module.docs computed property.

--- Source/SourceKittenFramework/Module.swift
+++ Source/SourceKittenFramework/Module.swift
@@ -11,19 +11,33 @@ public struct Module {
     public let sourceFiles: [String]
 
     /// Documentation for this Module. Typically expensive computed property.
+    /// (Does this really need to be computed every time?)
+    ///
     public var docs: [SwiftDocs] {
-        var fileIndex = 1
-        let sourceFilesCount = sourceFiles.count
-        return sourceFiles.sorted().compactMap {
-            let filename = $0.bridge().lastPathComponent
-            if let file = File(path: $0) {
-                fputs("Parsing \(filename) (\(fileIndex)/\(sourceFilesCount))\n", stderr)
+        var fileIndex        = 1
+        let queue            = DispatchQueue(label: UUID().uuidString, qos: .userInitiated, attributes: .concurrent)
+        let group            = DispatchGroup()
+        let lock             = NSLock()
+        var out: [SwiftDocs] = []
+
+        for fn in sourceFiles.sorted() {
+            queue.async(group: group) {
+                guard let file = File(path: fn), let docs = SwiftDocs(file: file, arguments: compilerArguments) else {
+                    lock.lock()
+                    defer { lock.unlock() }
+                    fputs("Could not parse `\(fn.bridge().lastPathComponent)`. Please open an issue at https://github.com/jpsim/SourceKitten/issues with the file contents.\n", stderr)
+                    return
+                }
+                lock.lock()
+                defer { lock.unlock() }
+                out.append(docs)
+                fputs("Parsed \(fn.bridge().lastPathComponent) (\(fileIndex)/\(sourceFiles.count))\n", stderr)
                 fileIndex += 1
-                return SwiftDocs(file: file, arguments: compilerArguments)
             }
-            fputs("Could not parse `\(filename)`. Please open an issue at https://github.com/jpsim/SourceKitten/issues with the file contents.\n", stderr)
-            return nil
         }
+
+        group.wait()
+        return out
     }
 
     /**
