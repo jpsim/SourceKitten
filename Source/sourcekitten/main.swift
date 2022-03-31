ea4e07c2e5a6c60013f8371adbae96d38d0fb672
Maxwell Elliott <maxwell.elliott@gotinder.com>
2022-03-31 15:36:28 -0700

    suppress error in main

--- Source/sourcekitten/main.swift
+++ Source/sourcekitten/main.swift
@@ -10,9 +10,12 @@ import Dispatch
 
 // `sourcekitd_set_notification_handler()` sets the handler to be executed on main thread queue.
 // So, we vacate main thread to `dispatchMain()`.
-DispatchQueue.global(qos: .default).async {
+if #available(macOS 10.10, *) {
+    DispatchQueue.global(qos: .default).async {
+        SourceKitten.main()
+        exit(0)
+    }
+    dispatchMain()
+} else {
     SourceKitten.main()
-    exit(0)
 }
-
-dispatchMain()
