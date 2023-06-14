fb64d42d22f4c7b0270df042e3e82d2bd073ae2b
JP Simard <jp@jpsim.com>
2023-06-14 10:04:57 -0400

    build: bump SWXMLHash to 7.0.2 (#778)
    The 7.0.2 release introduces support for Windows, which is needed to
    support SourceKitten on Windows.  Bump the dependency minimum version.
    
    Co-authored-by: Saleem Abdulrasool <compnerd@compnerd.org>

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "1.2.1")),
-        .package(url: "https://github.com/drmohundro/SWXMLHash.git", .upToNextMinor(from: "7.0.1")),
+        .package(url: "https://github.com/drmohundro/SWXMLHash.git", .upToNextMinor(from: "7.0.2")),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.5"),
     ],
     targets: [
